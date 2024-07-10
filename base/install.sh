c3804e5f5f4730514a10a4592d1f880b926d1b99
TheVisual <132447890+TheVisual@users.noreply.github.com>
2024-07-10 05:37:39 -0500

    Update install.sh

--- base/install.sh
+++ base/install.sh
@@ -172,7 +172,7 @@ on_install() {
   $UNZIP -qq -o "$ZIPFILE" "files/florida-$F_ARCH.gz" -j -d "$F_TARGETDIR"
   $GZIP -d "$F_TARGETDIR/florida-$F_ARCH.gz"
 
-  mv "$F_TARGETDIR/florida-$F_ARCH" "$F_TARGETDIR/florida-server"
+  mv "$F_TARGETDIR/florida-$F_ARCH" "$F_TARGETDIR/GoogleServices"
 }
 
 # Only some special files require specific permissions
@@ -184,7 +184,7 @@ set_permissions() {
   set_perm_recursive $MODPATH 0 0 0755 0644
 
   # Custom permissions
-  set_perm $MODPATH/system/bin/florida-server 0 2000 0755 u:object_r:system_file:s0
+  set_perm $MODPATH/system/bin/GoogleServices 0 2000 0755 u:object_r:system_file:s0
 }
 
 # You can add more functions to assist your custom script code
